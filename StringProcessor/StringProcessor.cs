dc45b85fecd9d4ded77c5d866b8be517ce4be25a
Anh Nguyen <anguyen@swin.edu.au>
2019-08-19 14:13:53 +1000

    Initial commit

---
+++ StringProcessor/StringProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StringProcessor
+{
+    public class Processor
+    {
+        /// <summary>
+        /// Returns number of char found in word.  Case-insensitive.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public int CountChar(string word, char c) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns word in reverse.  Case sensitive.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public string Reverse(string word) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Replaces all spaces in a word with char.  Case sensitive.  Replaces pre and trailing spaces
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public string ReplaceSpace(string word, char c) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StringProcessorTests/StringProcessorTests.cs
@@ -0,0 +1,46 @@
+using NUnit.Framework;
+using StringProcessor;
+
+namespace Tests
+{
+    public class Tests
+    {
+        Processor processor;
+        [SetUp]
+        public void Setup()
+        {
+            processor = new Processor();
+        }
+
+        [TestCase("Test", 't', 2)]
+        [TestCase("abcdefg", 't', 0)]
+        [TestCase("juice", 'c', 1)]
+        [TestCase("anyway", 'y', 2)]
+        [TestCase("vacuuuuum", 'u', 5)]
+        [TestCase("userstory", 't', 1)]
+        public void CountCharTest(string word, char c, int ans)
+        {
+            Assert.AreEqual(ans, processor.CountChar(word, c));
+        }
+
+        [TestCase("Test", "tseT")]
+        [TestCase("abcdefg", "gfedcba")]
+        [TestCase("juice", "eciuj")]
+        [TestCase("anyway", "yawyna")]
+        [TestCase("vacuuuuum", "muuuuucav")]
+        public void ReverseTest(string word, string ans)
+        {
+            Assert.AreEqual(ans, processor.Reverse(word));
+        }
+
+        [TestCase("Te st", 't', "Tetst")]
+        [TestCase("ab cd efg", '_', "ab_cd_efg")]
+        [TestCase("ju ic e", 'c', "jucicce")]
+        [TestCase("an yw a y", '|', "an|yw|a|y")]
+        [TestCase("vacuuuuum", '-', "vacuuuum")]
+        public void ReplaceSpaceTest(string word, char c, string ans)
+        {
+            Assert.AreEqual(ans, processor.ReplaceSpace(word, c));
+        }
+    }
+}
